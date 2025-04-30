0188fc6d6006b002eac0832cd6d333120651e5a6
Abdul Rafeh 3333github <rimran-work@outlook.com>
2025-04-30 19:33:35 +0000

    Complteted task 1ii) used guards

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,16 +1,17 @@
---create inpFunc
+-- Create inpFunc
 inpFunc a b = [a..b]
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- Define applicatorFunc
+applicatorFunc inpFunc s
+    | s == 's' = (sum inpFunc)  -- Convert Int to Double
+    | otherwise = (sum inpFunc) / 5  -- Use Double division
 
 main = do
-    putStrLn "Gimme 2 numbers seperately"
+    putStrLn "Gimme 2 numbers separately"
     line1 <- getLine
     line2 <- getLine
-    let input1 = (read line1 )
-    let input2 = (read line2 ) -- all inputs now taken in, plug into a list:
+    let input1 = read line1   -- Explicit type annotation
+    let input2 = read line2 
     let list = inpFunc input1 input2
-    let result = applicatorFunc list 'a' --put that into result
+    let result = applicatorFunc list 's'
     putStrLn (show result)
- 
