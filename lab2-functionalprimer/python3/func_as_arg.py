9e36e0c49779cb0959ae869ae49e57ce0c59a71d
Abdul Rafeh 3333github <rimran-work@outlook.com>
2025-04-30 21:11:03 +0000

    Done task 2 in python

--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,37 @@
-def listFunc():
-    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+import time
 
-def applicatorFunc(inpFunc, s):
-    if s=='s':
-        return sum(inpFunc())
+#Function to make list sums easy
+def listSum(listInput):
+    x = 0
+    for i in listInput:
+        x+=i
+    return x
+
+def listFunc(a, b): #Function for making a list a-b
+    return [i for i in range(a, (b+1))] #Square brackets turn all into a list! Also, b+1 is not including
+
+
+
+def applicatorFunc(listParemter, charInput):
+    if charInput == "s": #s for sum
+        return (listSum(listParemter))
+    elif charInput == "quit":
+        time.sleep(0.5)
+        print("quitting (in 2 secs)...")
+        time.sleep(2)
+        quit()
     else:
-        return sum(inpFunc())/5
+        return (listSum(listParemter)/5)
 
-print(applicatorFunc(listFunc, 's'))
+#MAIN
+ #create two input variables and plug into a list
+i1 = int(input("Enter a starting number: "))
+i2 = int(input("Enter an ending number: "))
+ourList = listFunc(i1, i2)
+
+ourChar = input("Enter s for sum, anything else: ")
+
+ #print result
+print(applicatorFunc(ourList, ourChar))
+
+
